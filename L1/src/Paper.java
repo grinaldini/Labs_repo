bfed4a68bc2bd96936e9c311824bc310a2eb4361
Giancarlo Rinaldini <grinaldini@smu.edu>
2021-09-01 14:14:45 -0500

    Lab1 Setup
    DomParserDemo XML parser and database updates

---
+++ L1/src/Paper.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Paper {
+    String inproceedings;
+    Vector author = new Vector(3,2);
+    String title;
+    String pages;
+    String year;
+    String crossref;
+    String booktitle;
+    String url;
+    //For printing functions
+    //String printAuthor;
+    @Override
+    public String toString() {
+        //Enumeration vEnum = author.elements();
+        //while(vEnum.hasMoreElements())
+           // System.out.print(vEnum.nextElement()+" ");
+
+        return author+title+"("+inproceedings+")"+pages+year+crossref+booktitle+url;
+    }
+}
